4601519e3eab06c15c7581aaa45f2a9e7c0d67e3
Fahed <fahedissa_1990@hotmail.com>
2020-05-14 21:21:25 +0200

    stuff!

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import './App.css';
-import qtechgroup from '../src/1.png'
+import qtechgroup from '../src/1.png';
+import { Navbar } from './components/Navbar';
+import { MdAccountCircle } from "react-icons/md";
 
-import { CardBox } from './components/CardBox'
-import { CardBox2 } from './components/CardBox2' 
+
 
 function App() {
+  
+
   return (
     <div>
+     
       <header>
-      <img class = "img" src={qtechgroup} />
-        <CardBox />
        
+          <img class = "img" src={qtechgroup} />
+          <Navbar/>
+
+          <MdAccountCircle color ='green' size ='3rem'/>
+          <button onclick="Login()"> <MdAccountCircle color ='green' size ='3rem'/> </button>
+          
       </header>
+     
     </div>
   );
 }
