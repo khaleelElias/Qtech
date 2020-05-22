7f2c5546d624da38a3b3542f681b619099da3997
Fahed <fahedissa_1990@hotmail.com>
2020-05-22 20:17:09 +0200

    fixed bugs with the last merge!

--- src/components/User_Bar.js
+++ src/components/User_Bar.js
@@ -7,52 +7,26 @@ import { getWeekNumber } from '../constants/index'
 import { View_Persona } from './View_Persona' 
 import { Action_Menu } from './Action_Menu'
 
-export class CardBox extends React.Component{
-
-    constructor(props){
-        super(props)
-        this.state = {
-            data: []
-        }
-        this.loadData()
-    }
-
-    loadData = () => {
-        fetch('/users')
-        .then(Response => Response.json())
-        .then(res => {
-            console.log(res)
-            //this.setState({ data: [...this.state.data, ...res.users ]}) //create user
-            this.setState({data: [...res.users]})
-        }).catch( error => {
-            console.log("error: ", error)
-        })
-    }
-
+export class UserBar extends React.Component{
     render() {
-        console.log("data: " , this.state.data)
+        console.log("data: " , this.props.users)
         return(
             <div class = "header ">
                 <Stack horizontal>
                     <Stack>
                         <Stack horizontal>
                             {
-                                this.state.data.slice(0, 30).map( (value, index) => {
+                                this.props.users.slice(0, 30).map( (value, index) => {
                                     return (
                                        <View_Persona name={value.username} status={value.status} id={value.id} index={index} reloadData={this.loadData.bind(this)} />
                                     )
                                 })
                             }
-
-                            <Action_Menu reloadData={this.loadData.bind(this)} history={this.props.history} />
-                            
-
+                            <Action_Menu reloadData={this.props.loadData} history={this.props.history} />
                         </Stack>
                     </Stack>
                 </Stack>
-               
             </div>
         )
     }
-
 }
